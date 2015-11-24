5ce49615f71617722c57aabdc6b53b1545c61edf
Eric Chamberlin <echamberlin@me.com>
2015-11-24 15:11:33 +0100

    what I have done today...

---
+++ BlocSpot/BlocSpot/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  BlocSpot
+//
+//  Created by Eric Chamberlin on 11/24/15.
+//  Copyright © 2015 Eric Chamberlin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+
+class ViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    //starts map overlooking Pyrenees
+    let initialLocation = CLLocation(latitude: 42.988566, longitude: 0.460573)
+    
+    var searchController:UISearchController!
+    var annotation:MKAnnotation!
+    var localSearchRequest:MKLocalSearchRequest!
+    var localSearch:MKLocalSearch!
+    var localSearchResponse:MKLocalSearchResponse!
+    var error:NSError!
+    var pointAnnotation:MKPointAnnotation!
+    var pinAnnotationView:MKPinAnnotationView!
+    
+    // MARK: - location manager to authorize user location for Maps app
+    
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        centerMapOnLocation(initialLocation)
+        checkLocationAuthorizationStatus()
+        
+        let addSpot = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        addSpot.minimumPressDuration = 2
+        
+        mapView.addGestureRecognizer(addSpot)
+        
+    }
+    /*
+    Beginning to try to allow them to add a new place through touch
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        
+        var touchPoint = gestureRecognizer.locationInView(self.mapView)
+        
+        var newCoordinate = CLLocationCoordinate2D = mapView.convertPoint(touchPoint, fromView: self.mapView)
+        
+    }
+*/
+    let regionRadius: CLLocationDistance = 200000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+
+    @IBAction func searchButton(sender: UIBarButtonItem) {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        presentViewController(searchController, animated: true, completion: nil)
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar){
+        
+        searchBar.resignFirstResponder()
+        dismissViewControllerAnimated(true, completion: nil)
+        if self.mapView.annotations.count != 0{
+            annotation = self.mapView.annotations[0]
+            self.mapView.removeAnnotation(annotation)
+        }
+      
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.startWithCompletionHandler { (localSearchResponse, error) -> Void in
+            
+            if localSearchResponse == nil{
+                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+                return
+            }
+           
+            self.pointAnnotation = MKPointAnnotation()
+            self.pointAnnotation.title = searchBar.text
+            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+            
+            
+            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+            self.mapView.centerCoordinate = self.pointAnnotation.coordinate
+            self.mapView.addAnnotation(self.pinAnnotationView.annotation!)
+        }
+    }
+
+    //trying to customize the pin
+    /*
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if !(annotation is MKPointAnnotation) {
+            return nil
+        }
+        
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("demo")
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "demo")
+            annotationView!.canShowCallout = true
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        annotationView!.image = UIImage(named: "pin_for_map3")
+        
+        return annotationView
+        
+    }
+*/
+}
+
+
+
